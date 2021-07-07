eed48f9bd77274605c93b62dd4bf91cb255bfee2
wiselder <wiselder@ya.ru>
2021-07-07 15:52:49 +0300

    Backend initial commit

---
+++ src/main/java/ru/dsr/itest/ITestApplication.java
@@ -0,0 +1,13 @@
+package ru.dsr.itest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dsr/itest/ITestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dsr.itest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

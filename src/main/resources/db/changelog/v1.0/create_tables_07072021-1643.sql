f61bc56bdb8700c4a55aacd33584b25a8f0cf142
wiselder <wiselder@ya.ru>
2021-07-08 15:46:23 +0300

    Update properties files and addeed sql script

---
+++ src/main/resources/db/changelog/v1.0/create_tables_07072021-1643.sql
@@ -0,0 +1,67 @@
+CREATE TABLE IF NOT EXISTS discipline (
+    id INT PRIMARY KEY,
+    title VARCHAR(256) NOT NULL UNIQUE
+);
+INSERT INTO discipline (id, title) VALUES (0, 'JAVA') ON CONFLICT DO NOTHING;
+INSERT INTO discipline (id, title) VALUES (1, 'CSHARP') ON CONFLICT DO NOTHING;
+INSERT INTO discipline (id, title) VALUES (3, 'PYTHON') ON CONFLICT DO NOTHING;
+CREATE TABLE IF NOT EXISTS account (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(256) NOT NULL UNIQUE,
+    password VARCHAR(256) NOT NULL,
+    role INT DEFAULT 0
+);
+CREATE TABLE IF NOT EXISTS student (
+    account_id INT PRIMARY KEY REFERENCES account(id) ON DELETE CASCADE,
+    first_name VARCHAR(64),
+    middle_name VARCHAR(64),
+    last_name VARCHAR(64),
+    university VARCHAR(256),
+    faculty VARCHAR(256),
+    direction VARCHAR(256),
+    grade INT CHECK (grade > 0 AND grade < 8),
+    group_number INT CHECK (group_number > 0)
+);
+CREATE TABLE IF NOT EXISTS test (
+    id SERIAL PRIMARY KEY,
+    discipline_id INT NOT NULL REFERENCES discipline(id) ON DELETE CASCADE,
+    creator_id INT NOT NULL REFERENCES account(id) ON DELETE CASCADE,
+    title VARCHAR(256) NOT NULL DEFAULT 'New test',
+    time_start TIMESTAMP,
+    time_end TIMESTAMP,
+    duration INT
+);
+CREATE TABLE IF NOT EXISTS question (
+    id SERIAL PRIMARY KEY,
+    test_id INT NOT NULL REFERENCES test(id) ON DELETE CASCADE,
+    title VARCHAR(2048) NOT NULL DEFAULT 'New question',
+    weight INT NOT NULL DEFAULT 0
+);
+CREATE TABLE IF NOT EXISTS choice (
+    id SERIAL PRIMARY KEY,
+    question_id INT NOT NULL REFERENCES question(id) ON DELETE CASCADE,
+    title VARCHAR(256) NOT NULL DEFAULT 'New choice',
+    correct BOOLEAN DEFAULT FALSE
+);
+CREATE TABLE IF NOT EXISTS variant (
+    id SERIAL PRIMARY KEY,
+    test_id INT NOT NULL REFERENCES test(id) ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS variant_config (
+    variant_id INT REFERENCES variant(id) ON DELETE CASCADE,
+    question_id INT REFERENCES question(id) ON DELETE CASCADE,
+    PRIMARY KEY (variant_id, question_id)
+);
+CREATE TABLE IF NOT EXISTS response (
+    id SERIAL PRIMARY KEY,
+    respondent_id INT NOT NULL REFERENCES account(id) ON DELETE CASCADE,
+    test_id INT NOT NULL REFERENCES test(id) ON DELETE CASCADE,
+    variant_id INT NOT NULL REFERENCES variant(id) ON DELETE CASCADE,
+    time_start TIMESTAMP NOT NULL DEFAULT NOW(),
+    time_end TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS answer (
+    response_id INT NOT NULL REFERENCES response(id) ON DELETE CASCADE,
+    choice_id INT NOT NULL REFERENCES choice(id) ON DELETE CASCADE
+)
+
